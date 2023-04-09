a52e5234c33e6d1b934b0d61605bc125df724f2b
Pablo Alan <pabloxt14@gmail.com>
2023-04-09 15:54:17 -0300

    using join method to concat folder path on multerConfig.js

--- src/multerConfig.js
+++ src/multerConfig.js
@@ -1,15 +1,16 @@
 import { fileURLToPath } from 'node:url';
-import path from 'node:path';
+import path, { join } from 'node:path';
 import multer from 'multer';
 
 const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-const __dirname = new URL('.', import.meta.url).pathname;
-console.log(__dirname);
+const __dirname = path.dirname(__filename);
+
+// const uploadsFoder = join(__dirname, 'uploads');
+// console.log(uploadsFoder);
 
 const storage = multer.diskStorage({
   destination: (request, file, callback) => {// dizer para o multer onde salvar o arquivo
-    callback(null, path.resolve(__dirname, 'uploads'));// o primeiro parâmetro de todo callback no multer indica erro a retonar
+    callback(null, join(__dirname, 'uploads'));// o primeiro parâmetro de todo callback no multer indica erro a retonar
   },
   filename: (request, file, callback) => {// configuração de qual nome dar para o arquivo a ser salvo
     const time = new Date().getTime();
