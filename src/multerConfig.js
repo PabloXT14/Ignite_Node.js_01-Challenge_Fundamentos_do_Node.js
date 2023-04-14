83811ee4e9282791d8550e1504a2dde28ede2107
Pablo Alan <pabloxt14@gmail.com>
2023-04-14 16:18:54 -0300

    feat: add file .gitkeep on folder uploads for the folder to be created on Docker container

--- src/multerConfig.js
+++ src/multerConfig.js
@@ -10,7 +10,7 @@ const __dirname = path.dirname(__filename);
 
 const storage = multer.diskStorage({
   destination: (request, file, callback) => {// dizer para o multer onde salvar o arquivo
-    callback(null, join(__dirname, 'uploads'));// o primeiro parâmetro de todo callback no multer indica erro a retonar
+    callback(null, path.resolve(__dirname, 'uploads'));// o primeiro parâmetro de todo callback no multer indica erro a retonar
   },
   filename: (request, file, callback) => {// configuração de qual nome dar para o arquivo a ser salvo
     const time = new Date().getTime();
